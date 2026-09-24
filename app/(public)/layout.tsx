0d0a92bdcb26db7a5b21d01ad3b61cda16f3529b
kapil-dev14 <kapildevbelbase32@gmail.com>
2026-09-24 16:33:09 +0545

    done

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,20 +1,10 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
-  return (
-    <>
-      <Header />
-      <Analytics />
-      <main>{children}</main>
-      <Footer />
-    </>
-  );
 }>) {
   return (
     <>
